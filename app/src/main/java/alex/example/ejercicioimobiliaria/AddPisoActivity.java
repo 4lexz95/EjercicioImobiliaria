181dfb7008be361768a1ddd7f32ec52276b1a2c2
alex <alejandro.franchzapata@cieep.com>
2022-10-11 19:49:13 +0200

    revision

---
+++ app/src/main/java/alex/example/ejercicioimobiliaria/AddPisoActivity.java
@@ -0,0 +1,21 @@
+package alex.example.ejercicioimobiliaria;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.Serializable;
+
+import alex.example.ejercicioimobiliaria.databinding.ActivityAddPisoBinding;
+
+public class AddPisoActivity extends AppCompatActivity {
+    // Crear variable binding
+    private ActivityAddPisoBinding binding;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
